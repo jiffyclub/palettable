df9538cdaa6fad43bb1f8df43b98072102c3d2de
Matt Davis <jiffyclub.programatic@gmail.com>
2012-07-20 17:31:18 -0500

    Switched the version number. This is 1.0 level work.

--- brewer2mpl/__init__.py
+++ brewer2mpl/__init__.py
@@ -4,4 +4,4 @@ Connect colorbrewer2.org color maps to Python and matplotlib.
 """
 from .brewer2mpl import *
 
-__version__ = '0.1dev'
+__version__ = '1.0dev'
--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@
 from distutils.core import setup
 
 setup(name='brewer2mpl',
-      version='0.1dev',
+      version='1.0dev',
       packages=['brewer2mpl'],
       package_data={'brewer2mpl': ['data/colorbrewer*']})
