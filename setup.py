8bbd9598d6a68c339646ca0a82ba3c3490e4bc93
Matt Davis <jiffyclub.programatic@gmail.com>
2012-07-23 21:57:27 -0400

    Rounded out the setup.py and added an MIT license.

--- setup.py
+++ setup.py
@@ -3,5 +3,11 @@ from distutils.core import setup
 
 setup(name='brewer2mpl',
       version='1.0dev',
+      description='Connect colorbrewer2.org color maps to Python and matplotlib',
+      author='Matt Davis',
+      url='https://github.com/jiffyclub/brewer2mpl/wiki',
       packages=['brewer2mpl'],
-      package_data={'brewer2mpl': ['data/colorbrewer*']})
+      package_data={'brewer2mpl': ['data/colorbrewer*']},
+      classifiers=['License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python',
+                   'Topic :: Scientific/Engineering :: Visualization'])
