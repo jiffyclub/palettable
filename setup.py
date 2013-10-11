496eca0a051cbd96922b1b32168582250bb371e6
Matt Davis <jiffyclub@gmail.com>
2013-10-10 21:37:47 -0700

    load readme as long description in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
+with open('README.rst', 'r') as f:
+      long_description = f.read()
+
 setup(name='brewer2mpl',
       version='1.4.dev',
       description='Connect colorbrewer2.org color maps to Python and matplotlib',
+      long_description=long_description,
       author='Matt Davis',
       author_email='jiffyclub@gmail.com',
       url='https://github.com/jiffyclub/brewer2mpl/wiki',
