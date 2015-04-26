ebb069094c857f66f39144ae4d0efcec5271ae85
Jonathan Sick <jonathansick@mac.com>
2015-04-26 07:24:45 -0400

    Implement Cubehelix class
    The original cubehelix function is now a static method of Cubehelix so
    that it can provide a color list to the Palette baseclass.

--- palettable/cubehelix/__init__.py
+++ palettable/cubehelix/__init__.py
@@ -1,1 +1,3 @@
 from __future__ import absolute_import
+
+from .cubehelix import __doc__, Cubehelix
--- palettable/cubehelix/cubehelix.py
+++ palettable/cubehelix/cubehelix.py
@@ -42,32 +42,21 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 """
-from matplotlib.colors import LinearSegmentedColormap as LSC
-from math import pi
+from __future__ import absolute_import, print_function
+
 import numpy as np
 
+from ..palette import Palette
 
-def cmap(start=0.5, rot=-1.5, gamma=1.0, reverse=False, nlev=256.,
-         minSat=1.2, maxSat=1.2, minLight=0., maxLight=1.,
-         **kwargs):
+
+url = 'http://adsabs.harvard.edu/abs/2011arXiv1108.5083G'
+palette_type = 'sequential'
+
+
+class Cubehelix(Palette):
     """
-    A full implementation of Dave Green's "cubehelix" for Matplotlib.
-    Based on the FORTRAN 77 code provided in
-    D.A. Green, 2011, BASI, 39, 289.
-
-    http://adsabs.harvard.edu/abs/2011arXiv1108.5083G
-
-    User can adjust all parameters of the cubehelix algorithm.
-    This enables much greater flexibility in choosing color maps, while
-    always ensuring the color map scales in intensity from black
-    to white. A few simple examples:
-
-    Default color map settings produce the standard "cubehelix".
-
-    Create color map in only blues by setting rot=0 and start=0.
-
-    Create reverse (white to black) backwards through the rainbow once
-    by setting rot=1 and reverse=True.
+    Representation of a Cubehelix color map with matplotlib compatible
+    views of the map.
 
     Parameters
     ----------
@@ -105,74 +94,59 @@ def cmap(start=0.5, rot=-1.5, gamma=1.0, reverse=False, nlev=256.,
         Sets the minimum lightness value. Defaults to 0.
     maxLight : scalar, optional
         Sets the maximum lightness value. Defaults to 1.
-
-    Returns
-    -------
-    matplotlib.colors.LinearSegmentedColormap object
-
-    Example
-    -------
-    >>> import cubehelix
-    >>> import matplotlib.pyplot as plt
-    >>> import numpy as np
-    >>> x = np.random.randn(1000)
-    >>> y = np.random.randn(1000)
-    >>> cx = cubehelix.cmap(start=0., rot=-0.5)
-    >>> plt.hexbin(x, y, gridsize=50, cmap=cx)
-
-    Revisions
-    ---------
-    2014-04 (@jradavenport) Ported from IDL version
-    2014-04 (@jradavenport) Added kwargs to enable similar to D3 version,
-                            changed name of "hue" parameter to "sat"
     """
+    url = url
 
-# override start and rot if startHue and endHue are set
-    if kwargs is not None:
-        if 'startHue' in kwargs:
-            start = (kwargs.get('startHue') / 360. - 1.) * 3.
-        if 'endHue' in kwargs:
-            rot = kwargs.get('endHue') / 360. - start / 3. - 1.
-        if 'sat' in kwargs:
-            minSat = kwargs.get('sat')
-            maxSat = kwargs.get('sat')
+    def __init__(self, name="custom_cubehelix", **kwargs):
+        colors = Cubehelix._cubehelix(**kwargs)
+        super(Cubehelix, self).__init__(name, palette_type, colors)
 
-# set up the parameters
-    fract = np.linspace(minLight, maxLight, nlev)
-    angle = 2.0 * pi * (start / 3.0 + rot * fract + 1.)
-    fract = fract**gamma
+    @staticmethod
+    def _cubehelix(start=0.5, rot=-1.5, gamma=1.0, reverse=False, nlev=256.,
+                   minSat=1.2, maxSat=1.2, minLight=0., maxLight=1., **kwargs):
+        """
+        Create a sequence of RGB colours given cubehelix algorithm parameters.
 
-    satar = np.linspace(minSat, maxSat, nlev)
-    amp = satar * fract * (1. - fract) / 2.
+        """
+        # override start and rot if startHue and endHue are set
+        if kwargs is not None:
+            if 'startHue' in kwargs:
+                start = (kwargs.get('startHue') / 360. - 1.) * 3.
+            if 'endHue' in kwargs:
+                rot = kwargs.get('endHue') / 360. - start / 3. - 1.
+            if 'sat' in kwargs:
+                minSat = kwargs.get('sat')
+                maxSat = kwargs.get('sat')
 
-# compute the RGB vectors according to main equations
-    red = fract + amp * (-0.14861 * np.cos(angle) + 1.78277 * np.sin(angle))
-    grn = fract + amp * (-0.29227 * np.cos(angle) - 0.90649 * np.sin(angle))
-    blu = fract + amp * (1.97294 * np.cos(angle))
+        # set up the parameters
+        fract = np.linspace(minLight, maxLight, nlev)
+        angle = 2.0 * np.pi * (start / 3.0 + rot * fract + 1.)
+        fract = fract**gamma
 
-# find where RBB are outside the range [0,1], clip
-    red[np.where((red > 1.))] = 1.
-    grn[np.where((grn > 1.))] = 1.
-    blu[np.where((blu > 1.))] = 1.
+        satar = np.linspace(minSat, maxSat, nlev)
+        amp = satar * fract * (1. - fract) / 2.
 
-    red[np.where((red < 0.))] = 0.
-    grn[np.where((grn < 0.))] = 0.
-    blu[np.where((blu < 0.))] = 0.
+        # compute the RGB vectors according to main equations
+        cos_angle = np.cos(angle)
+        sin_angle = np.sin(angle)
+        red = fract + amp * (-0.14861 * cos_angle + 1.78277 * sin_angle)
+        grn = fract + amp * (-0.29227 * cos_angle - 0.90649 * sin_angle)
+        blu = fract + amp * (1.97294 * cos_angle)
 
-# optional color reverse
-    if reverse is True:
-        red = red[::-1]
-        blu = blu[::-1]
-        grn = grn[::-1]
+        # find where RBB are outside the range [0,1], clip
+        red[np.where((red > 1.))] = 1.
+        grn[np.where((grn > 1.))] = 1.
+        blu[np.where((blu > 1.))] = 1.
 
-# put in to tuple & dictionary structures needed
-    rr = []
-    bb = []
-    gg = []
-    for k in range(0, int(nlev)):
-        rr.append((float(k) / (nlev - 1.), red[k], red[k]))
-        bb.append((float(k) / (nlev - 1.), blu[k], blu[k]))
-        gg.append((float(k) / (nlev - 1.), grn[k], grn[k]))
+        red[np.where((red < 0.))] = 0.
+        grn[np.where((grn < 0.))] = 0.
+        blu[np.where((blu < 0.))] = 0.
 
-    cdict = {'red': rr, 'blue': bb, 'green': gg}
-    return LSC('cubehelix_map', cdict)
+        # optional color reverse
+        if reverse is True:
+            red = red[::-1]
+            blu = blu[::-1]
+            grn = grn[::-1]
+
+        colors = zip(red * 255., grn * 255., blu * 255.)
+        return colors
