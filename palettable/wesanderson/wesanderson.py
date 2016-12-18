febe7ee2970641e00e39ca9332305db430549f0f
Matt Davis <jiffyclub@gmail.com>
2016-12-18 15:45:54 -0800

    Set a string constant in wesanderson.py

--- palettable/wesanderson/wesanderson.py
+++ palettable/wesanderson/wesanderson.py
@@ -11,6 +11,7 @@ from ..palette import Palette
 
 
 _tumblr_template = 'http://wesandersonpalettes.tumblr.com/post/{0}'
+_palette_type = 'qualitative'
 
 # Tumblr palettes in chronological order
 _palettes = {
@@ -18,7 +19,7 @@ _palettes = {
         'colors': [
             (53, 82, 67), (254, 202, 73), (201, 213, 213), (187, 162, 137)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format('79263620764/hotel-chevalier')
     },
     'Moonrise1': {
@@ -26,7 +27,7 @@ _palettes = {
             (114, 202, 221), (240, 165, 176), (140, 133, 54), (195, 180, 119),
             (250, 208, 99)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '79263667140/sam-i-love-you-but-you-dont-know-what-youre')
     },
@@ -34,7 +35,7 @@ _palettes = {
         'colors': [
             (222, 141, 185), (184, 192, 246), (207, 147, 135), (92, 128, 204)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format('79348206200/mendls-heaven')
     },
     'Margot1': {
@@ -42,7 +43,7 @@ _palettes = {
             (137, 119, 18), (243, 194, 164), (246, 159, 151), (254, 214, 140),
             (98, 144, 117)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format('79348364517/margot-takes-a-bath')
     },
     'Cavalcanti': {
@@ -50,7 +51,7 @@ _palettes = {
             (209, 170, 0), (8, 50, 19), (146, 148, 96), (111, 152, 121),
             (132, 33, 17)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '79348553036/castello-cavalcanti-how-can-i-help')
     },
@@ -58,7 +59,7 @@ _palettes = {
         'colors': [
             (102, 124, 116), (181, 106, 39), (194, 186, 124), (31, 25, 23)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '79641731527/sam-why-do-you-always-use-binoculars-suzy-it')
     },
@@ -66,14 +67,14 @@ _palettes = {
         'colors': [
             (118, 139, 147), (188, 36, 15), (249, 236, 197), (212, 115, 41)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format('79641785036/margot-takes-a-break')
     },
     'Moonrise3': {
         'colors': [
             (242, 218, 82), (197, 157, 0), (203, 203, 201), (27, 30, 20)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '79783357790/suzy-ive-always-wanted-to-be-an-orphan-most-of')
     },
@@ -81,7 +82,7 @@ _palettes = {
         'colors': [
             (238, 174, 101), (251, 79, 85), (72, 19, 19), (204, 95, 39)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format('79784389334/the-grand-budapest-hotel')
     },
     'Moonrise4': {
@@ -89,7 +90,7 @@ _palettes = {
             (123, 135, 97), (193, 166, 46), (79, 143, 107), (59, 69, 60),
             (159, 50, 8)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format('79956897654/coming-soon')
     },
     'Zissou': {
@@ -97,7 +98,7 @@ _palettes = {
             (0, 153, 230), (18, 37, 90), (242, 56, 20), (223, 183, 139),
             (182, 195, 197)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '79956949771/steve-zissou-dont-point-that-gun-at-him-hes-an')
     },
@@ -105,7 +106,7 @@ _palettes = {
         'colors': [
             (121, 164, 58), (242, 214, 175), (94, 72, 41), (24, 20, 1)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '79957796915/royal-o-reilly-tenenbaum-1932-2001')
     },
@@ -113,7 +114,7 @@ _palettes = {
         'colors': [
             (158, 151, 151), (194, 142, 0), (131, 102, 89), (156, 90, 51)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '80149649946/jack-i-wonder-if-the-three-of-us-wouldve-been')
     },
@@ -122,7 +123,7 @@ _palettes = {
             (249, 219, 32), (147, 75, 78), (66, 23, 13), (194, 121, 34),
             (226, 200, 167)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '80149872170/mrs-fox-you-know-you-really-are-fantastic-mr')
     },
@@ -130,7 +131,7 @@ _palettes = {
         'colors': [
             (135, 162, 164), (202, 160, 101), (214, 202, 191), (214, 160, 160)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '109473707895/etheline-raleigh-says-youve-been-spending-six')
     },
@@ -138,7 +139,7 @@ _palettes = {
         'colors': [
             (255, 166, 142), (251, 204, 183), (140, 17, 8), (41, 11, 4)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '109473911685/m-gustave-you-see-there-are-still-faint')
     },
@@ -147,7 +148,7 @@ _palettes = {
             (52, 36, 25), (28, 64, 39), (241, 201, 14), (102, 88, 153),
             (184, 147, 130)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '109568074320/steve-zissou-the-deeper-you-go-the-weirder-life')
     },
@@ -156,7 +157,7 @@ _palettes = {
             (213, 227, 216), (97, 138, 152), (249, 218, 149), (174, 75, 22),
             (120, 112, 100)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format('109980167015/peter-fuck-the-itinerary')
     },
     'FantasticFox2': {
@@ -164,7 +165,7 @@ _palettes = {
             (228, 191, 68), (198, 87, 66), (154, 208, 187), (51, 39, 55),
             (171, 161, 141)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format('110716093015/ash-should-we-dance')
     },
     'GrandBudapest3': {
@@ -172,7 +173,7 @@ _palettes = {
             (255, 220, 182), (37, 56, 69), (231, 173, 157), (102, 117, 110),
             (139, 63, 49), (150, 109, 53)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '112305028860/m-gustave-mendls-is-the-best')
     },
@@ -181,7 +182,7 @@ _palettes = {
             (194, 171, 186), (140, 59, 73), (182, 172, 166), (33, 32, 83),
             (209, 211, 213)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '115124780615/royal-anybody-interested-in-grabbing-a-couple-of')
     },
@@ -190,7 +191,7 @@ _palettes = {
             (223, 140, 144), (216, 210, 142), (245, 190, 37), (61, 74, 28),
             (209, 48, 96), (168, 107, 76)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '116909186645/walt-bishop-our-daughters-been-abducted-by-one')
     },
@@ -199,7 +200,7 @@ _palettes = {
             (186, 110, 0), (80, 32, 86), (255, 246, 187), (154, 127, 25),
             (31, 18, 27)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '117849683385/concierge-and-how-long-will-you-be-staying-with')
     },
@@ -208,7 +209,7 @@ _palettes = {
             (191, 81, 61), (201, 162, 150), (197, 193, 136), (123, 177, 145),
             (217, 174, 48)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '118877161325/sam-im-not-that-strong-of-a-swimmer-so-i-wear-a')
     },
@@ -217,7 +218,7 @@ _palettes = {
             (225, 146, 131), (140, 27, 76), (209, 147, 54), (231, 199, 190),
             (51, 12, 0)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '122169507295/m-gustave-its-quite-a-thing-winning-the-loyalty')
     },
@@ -226,7 +227,7 @@ _palettes = {
             (139, 156, 184), (233, 229, 65), (88, 159, 194), (160, 141, 94),
             (189, 185, 206)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '125170837755/steve-zissou-please-dont-make-fun-of-me-i-just')
     },
@@ -235,7 +236,7 @@ _palettes = {
             (252, 87, 108), (237, 126, 83), (226, 153, 139), (46, 23, 24),
             (251, 214, 202)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '129921576355/royal-ive-always-been-considered-an-asshole-for')
     },
@@ -244,7 +245,7 @@ _palettes = {
             (35, 35, 85), (97, 104, 96), (167, 91, 68), (145, 43, 41),
             (255, 227, 143)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '137096576550/social-services-access-denied')
     },
@@ -253,7 +254,7 @@ _palettes = {
             (214, 161, 66), (194, 128, 114), (200, 183, 161), (189, 68, 45),
             (100, 84, 60)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '139482629630/ah-weve-never-made-great-husbands-have-we-of')
     },
@@ -262,7 +263,7 @@ _palettes = {
             (168, 171, 80), (255, 232, 61), (169, 211, 210), (36, 71, 125),
             (90, 145, 124)
         ],
-        'type': 'qualitative',
+        'type': _palette_type,
         'url': _tumblr_template.format(
             '143938510215/the-chief-steward-welcome-aboard')
     }
