aeeed6e4ad0d9dc958e71a606e374eb17d8e7fbd
Andy Eschbacher <andy.eschbacher@gmail.com>
2017-08-15 10:06:40 -0400

    adds qualitative tests

--- palettable/cartocolors/test/test_cartocolors.py
+++ palettable/cartocolors/test/test_cartocolors.py
@@ -13,6 +13,13 @@ def test_print_maps_diverging(capsys):
     assert lines[0] == 'ArmyRose_2    diverging       2'
 
 
+def test_print_maps_qualitative(capsys):
+    cartocolors.qualitative.print_maps()
+    out, err = capsys.readouterr()
+    lines = out.split('\n')
+    assert lines[0] == 'Antique_2     qualitative     2'
+
+
 def test_print_maps_sequential(capsys):
     cartocolors.sequential.print_maps()
     out, err = capsys.readouterr()
@@ -25,6 +32,15 @@ def test_get_map_diverging():
     assert isinstance(palette, cartocolors.cartocolorspalette.CartoColorsMap)
     assert palette.name == 'Geyser_5'
     assert palette.type == 'diverging'
+    assert len(palette.colors) == 5
+    assert palette.url == 'https://github.com/CartoDB/CartoColor/wiki/CARTOColor-Scheme-Names'
+
+
+def test_get_map_qualitative():
+    palette = cartocolors.qualitative.get_map('Bold_5')
+    assert isinstance(palette, cartocolors.cartocolorspalette.CartoColorsMap)
+    assert palette.name == 'Bold_5'
+    assert palette.type == 'qualitative'
     assert len(palette.colors) == 5
     assert palette.url == 'https://github.com/CartoDB/CartoColor/wiki/CARTOColor-Scheme-Names'
 
@@ -47,6 +63,15 @@ def test_get_map_diverging_reversed():
     assert palette.url == 'https://github.com/CartoDB/CartoColor/wiki/CARTOColor-Scheme-Names'
 
 
+def test_get_map_qualitative_reversed():
+    palette = cartocolors.qualitative.get_map('Safe_3_r', reverse=True)
+    assert isinstance(palette, cartocolors.cartocolorspalette.CartoColorsMap)
+    assert palette.name == 'Safe_3_r'
+    assert palette.type == 'qualitative'
+    assert len(palette.colors) == 3
+    assert palette.url == 'https://github.com/CartoDB/CartoColor/wiki/CARTOColor-Scheme-Names'
+
+
 def test_get_map_sequential_reversed():
     palette = cartocolors.sequential.get_map('Mint_5', reverse=True)
     assert isinstance(palette, cartocolors.cartocolorspalette.CartoColorsMap)
@@ -63,6 +88,12 @@ def test_palettes_loaded():
                       cartocolors.cartocolorspalette.CartoColorsMap)
     assert cartocolors.diverging.Earth_7.type == 'diverging'
 
+    assert isinstance(cartocolors.qualitative.Vivid_4,
+                      cartocolors.cartocolorspalette.CartoColorsMap)
+    assert isinstance(cartocolors.qualitative.Vivid_4_r,
+                      cartocolors.cartocolorspalette.CartoColorsMap)
+    assert cartocolors.qualitative.Vivid_4.type == 'qualitative'
+
     assert isinstance(cartocolors.sequential.BluGrn_7,
                       cartocolors.cartocolorspalette.CartoColorsMap)
     assert isinstance(cartocolors.sequential.BluGrn_7_r,
@@ -75,6 +106,10 @@ def test_get_all_maps():
     assert isinstance(
         utils.load_all_palettes(cartocolors.diverging._NAMES_AND_LENGTHS,
                                 cartocolors.diverging.get_map),
+        dict)
+    assert isinstance(
+        utils.load_all_palettes(cartocolors.qualitative._NAMES_AND_LENGTHS,
+                                cartocolors.qualitative.get_map),
         dict)
     assert isinstance(
         utils.load_all_palettes(cartocolors.sequential._NAMES_AND_LENGTHS,
